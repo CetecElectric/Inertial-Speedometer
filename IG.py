3bd62f6b63cf3d8c8b7cf3c7f1d2adba29a8c8e0
DracoLab <quyifei2024@i.pkuschool.edu.cn>
2026-03-16 16:18:18 +0800

    Update IG.py

--- IG.py
+++ IG.py
@@ -1,25 +1,33 @@
 import csv
 
+NAME = 'Example.csv' # Input file name
+
 def deltaV(i):
     global raw, bias
     tmp0 = raw[i-1].split(',')
     tmp1 = raw[i].split(',')
     return (float(tmp1[0]) - float(tmp0[0])) * (float(tmp1[2]) - bias)
+    # Number in the bracket may be changed according to the axis used.
+    # tmp1[1] is x-axis, tmp1[2] is y-axis, and tmp1[3] is z-axis.
 
 data_x = []
 data_y = []
 
 import matplotlib.pyplot as plt
 
-f = open("武汉-红安西.csv")
+f = open(NAME)
 raw = f.readlines()
 n = len(raw)
 spd = 0
 bias = 0
 
+# Measure bias
+# Bias measurement typically takes 3~6 seconds
 for i in range(500,1500):
     tmp = raw[i].split(',')
     bias += float(tmp[2])
+    # Number in the bracket may be changed according to the axis used.
+    # tmp1[1] is x-axis, tmp1[2] is y-axis, and tmp1[3] is z-axis.
 bias /= 1000
 bias += 0.026
 print('bias = ', bias)
